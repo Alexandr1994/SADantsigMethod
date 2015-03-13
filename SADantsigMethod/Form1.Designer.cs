6ead3eec50b14c8b80be3dcd45482f44bb62594e
Alexandr Fakhrutdinov <alexfahat@gmail.com>
2015-03-14 01:37:36 +0600

    Upload project.

---
+++ SADantsigMethod/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace SADantsigMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainBox = new System.Windows.Forms.GroupBox();
+            this.statusBox = new System.Windows.Forms.GroupBox();
+            this.controlBox = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.controlBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainBox
+            // 
+            this.mainBox.Location = new System.Drawing.Point(12, 12);
+            this.mainBox.Name = "mainBox";
+            this.mainBox.Size = new System.Drawing.Size(399, 299);
+            this.mainBox.TabIndex = 0;
+            this.mainBox.TabStop = false;
+            this.mainBox.Text = "Функция и ограничения";
+            // 
+            // statusBox
+            // 
+            this.statusBox.Location = new System.Drawing.Point(417, 12);
+            this.statusBox.Name = "statusBox";
+            this.statusBox.Size = new System.Drawing.Size(211, 106);
+            this.statusBox.TabIndex = 1;
+            this.statusBox.TabStop = false;
+            this.statusBox.Text = "Статус";
+            // 
+            // controlBox
+            // 
+            this.controlBox.Controls.Add(this.label2);
+            this.controlBox.Controls.Add(this.label1);
+            this.controlBox.Controls.Add(this.button2);
+            this.controlBox.Controls.Add(this.button1);
+            this.controlBox.Location = new System.Drawing.Point(417, 124);
+            this.controlBox.Name = "controlBox";
+            this.controlBox.Size = new System.Drawing.Size(211, 123);
+            this.controlBox.TabIndex = 2;
+            this.controlBox.TabStop = false;
+            this.controlBox.Text = "Управление";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Решить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Отобразить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Решить задачу \r\nпоиска максимума";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Отобразить\r\n решение\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 323);
+            this.Controls.Add(this.controlBox);
+            this.Controls.Add(this.statusBox);
+            this.Controls.Add(this.mainBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.controlBox.ResumeLayout(false);
+            this.controlBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox mainBox;
+        private System.Windows.Forms.GroupBox statusBox;
+        private System.Windows.Forms.GroupBox controlBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
