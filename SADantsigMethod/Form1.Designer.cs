300fd7374a52f51925f003fbb62c947d0a643546
Alexandr Fakhrutdinov <alexfahat@gmail.com>
2015-03-25 03:34:21 +0600

    Create programm graphic interface to input source data

--- SADantsigMethod/Form1.Designer.cs
+++ SADantsigMethod/Form1.Designer.cs
@@ -28,28 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.mainBox = new System.Windows.Forms.GroupBox();
+            this.dataBox = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.varAddBtn = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.resAddBtn = new System.Windows.Forms.Button();
             this.statusBox = new System.Windows.Forms.GroupBox();
             this.controlBox = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.controlBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // mainBox
+            // dataBox
             // 
-            this.mainBox.Location = new System.Drawing.Point(12, 12);
-            this.mainBox.Name = "mainBox";
-            this.mainBox.Size = new System.Drawing.Size(399, 299);
-            this.mainBox.TabIndex = 0;
-            this.mainBox.TabStop = false;
-            this.mainBox.Text = "Функция и ограничения";
+            this.dataBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataBox.Location = new System.Drawing.Point(12, 12);
+            this.dataBox.Name = "dataBox";
+            this.dataBox.Size = new System.Drawing.Size(393, 298);
+            this.dataBox.TabIndex = 0;
+            this.dataBox.TabStop = false;
+            this.dataBox.Text = "Функция и ограничения";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(7, 45);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(126, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Добавить переменную:";
+            // 
+            // varAddBtn
+            // 
+            this.varAddBtn.Location = new System.Drawing.Point(140, 41);
+            this.varAddBtn.Name = "varAddBtn";
+            this.varAddBtn.Size = new System.Drawing.Size(33, 21);
+            this.varAddBtn.TabIndex = 11;
+            this.varAddBtn.Text = "+";
+            this.varAddBtn.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(127, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Добавить ограничение:";
+            // 
+            // resAddBtn
+            // 
+            this.resAddBtn.Location = new System.Drawing.Point(140, 65);
+            this.resAddBtn.Name = "resAddBtn";
+            this.resAddBtn.Size = new System.Drawing.Size(33, 21);
+            this.resAddBtn.TabIndex = 3;
+            this.resAddBtn.Text = "+";
+            this.resAddBtn.UseVisualStyleBackColor = true;
             // 
             // statusBox
             // 
-            this.statusBox.Location = new System.Drawing.Point(417, 12);
+            this.statusBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.statusBox.Location = new System.Drawing.Point(411, 12);
             this.statusBox.Name = "statusBox";
             this.statusBox.Size = new System.Drawing.Size(211, 106);
             this.statusBox.TabIndex = 1;
@@ -58,61 +102,67 @@
             // 
             // controlBox
             // 
+            this.controlBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.controlBox.Controls.Add(this.label8);
+            this.controlBox.Controls.Add(this.varAddBtn);
             this.controlBox.Controls.Add(this.label2);
             this.controlBox.Controls.Add(this.label1);
+            this.controlBox.Controls.Add(this.label7);
+            this.controlBox.Controls.Add(this.resAddBtn);
             this.controlBox.Controls.Add(this.button2);
             this.controlBox.Controls.Add(this.button1);
-            this.controlBox.Location = new System.Drawing.Point(417, 124);
+            this.controlBox.Location = new System.Drawing.Point(411, 124);
             this.controlBox.Name = "controlBox";
-            this.controlBox.Size = new System.Drawing.Size(211, 123);
+            this.controlBox.Size = new System.Drawing.Size(211, 187);
             this.controlBox.TabIndex = 2;
             this.controlBox.TabStop = false;
             this.controlBox.Text = "Управление";
             // 
-            // button1
+            // label2
             // 
-            this.button1.Location = new System.Drawing.Point(6, 32);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Решить";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Отобразить\r\n решение\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Решить задачу \r\nпоиска максимума";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(6, 77);
+            this.button2.Location = new System.Drawing.Point(6, 146);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(89, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Отобразить";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // button1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(100, 29);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 26);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Решить задачу \r\nпоиска максимума";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(101, 77);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 26);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Отобразить\r\n решение\r\n";
+            this.button1.Location = new System.Drawing.Point(6, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Решить";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(640, 323);
+            this.ClientSize = new System.Drawing.Size(634, 322);
             this.Controls.Add(this.controlBox);
             this.Controls.Add(this.statusBox);
-            this.Controls.Add(this.mainBox);
+            this.Controls.Add(this.dataBox);
+            this.MaximumSize = new System.Drawing.Size(1200, 700);
             this.Name = "Form1";
             this.Text = "Form1";
             this.controlBox.ResumeLayout(false);
@@ -123,13 +173,17 @@
 
         #endregion
 
-        private System.Windows.Forms.GroupBox mainBox;
+        private System.Windows.Forms.GroupBox dataBox;
         private System.Windows.Forms.GroupBox statusBox;
         private System.Windows.Forms.GroupBox controlBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button resAddBtn;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button varAddBtn;
     }
 }
 
--- SADantsigMethod/Form1.cs
+++ SADantsigMethod/Form1.cs
@@ -12,9 +12,112 @@ namespace SADantsigMethod
 {
     public partial class Form1 : Form
     {
+
+        List<List<double>> coefficArray = new List<List<double>>();//двумерный динамический массив коэффициентов
+        List<List<TextBox>> textBoxArray = new List<List<TextBox>>();//двумерный динамический массив TextBox-ов коэффициентов
+        int restrictionNum=2;//количество ограничений
+        int varNum=3;//количество переменных
+
         public Form1()
         {
             InitializeComponent();
+            intiDataInterface();
         }
+
+
+        private void intiDataInterface()//инициализация интерфейса для работы с данными
+        {
+            varAddBtn.Click += addNewVar;
+            resAddBtn.Click += addNewRestricton;
+            renderDataInterface();
+        }
+
+
+        private void addNewVar(object sender, EventArgs e)//добавление переменной
+        {
+            varNum++;//увеличение числа заполняемых переменных
+            renderDataInterface();//перерисовка интерфейса
+        }
+
+        private void addNewRestricton(object sender, EventArgs e)//добавление ограничения
+        {
+            restrictionNum++;//увеличение числа ограничений
+            renderDataInterface();//перерисовка интерфейса
+        }
+
+        private void renderDataInterface()//отрисовка интерфейса для работы с данными
+        {
+            dataBox.Controls.Clear();//отчистить groupbox
+            addLabel(dataBox, 5, 30, "Функция:");
+            addLabel(dataBox, 5, 95, "Ограничения:");
+            textBoxArray.Clear();//отчистить массив форм 
+            renderFunctionLine();//отобразить формы функции
+            renderRestrictionForms();//отобразить формы ограничений
+            
+ 
+        }
+
+        public void renderFunctionLine()//отобразить формы функции и ряд столбцов переменных
+        {
+            addLabel(dataBox, 5, 60, "f(x)");
+            textBoxArray.Add(addCoefLine(25, 60));
+            addLabelsLine();
+        }
+
+        public void renderRestrictionForms()//отобразить наборы форм для ограничений
+        {
+            for (int i = 0; i < restrictionNum; i++)//постороение маблицы полей для ограничений
+            {
+                addLabel(dataBox, 5, 130 + (i * 20), (i + 1).ToString());
+                textBoxArray.Add(addCoefLine(25, 130 + (i * 20)));
+            }
+        }
+
+        private void addLabelsLine()//отобразить ряд подписей переменных
+        {
+            for (int i = 0; i < varNum; i++)
+            {
+                addLabel(dataBox, textBoxArray[0][i].Left + 10, textBoxArray[0][i].Top - 15, "x" + (i + 1));
+            }
+        }
+
+        private List<TextBox> addCoefLine(int coordX, int coordY)//добавить набор текстбоксов для ограничения или функции
+        {
+            List<TextBox> localCoefLine = new List<TextBox>();
+            for (int i = 0; i < varNum; i++)//ряд текстбоксов для коэффициентов
+            {
+                if (i > 0)
+                {
+                    localCoefLine.Add(addTextBox(dataBox, 40, localCoefLine[i - 1].Left + 40, localCoefLine[i - 1].Top));
+                }
+                else
+                {
+                    localCoefLine.Add(addTextBox(dataBox, 40, coordX, coordY));
+                }
+            }
+            addLabel(dataBox, localCoefLine[varNum - 1].Left + 40, localCoefLine[varNum - 1].Top, "=");//знак "="
+            localCoefLine.Add(addTextBox(dataBox, 40, localCoefLine[varNum - 1].Left + 60, localCoefLine[varNum - 1].Top));//
+            return localCoefLine;
+        }
+
+        private TextBox addTextBox(Control Controller, int weidth,int CoordX, int CoordY)//добавить поле для ввода коэфициента
+        {
+            TextBox newBox = new TextBox();
+            newBox.Size = new Size(weidth, 20);
+            newBox.Location = new Point(CoordX, CoordY);
+            Controller.Controls.Add(newBox);
+            return newBox;
+        }
+
+        private Label addLabel(Control Controller, int CoordX, int CoordY, String text)//добавить надпись
+        {
+            Label newLabel = new Label();
+            newLabel.Location = new Point(CoordX, CoordY);
+            newLabel.Text = text;
+            newLabel.AutoSize = true;
+            Controller.Controls.Add(newLabel);
+            return newLabel;
+        }
+
     }
 }
